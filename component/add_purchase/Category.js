91fef58dbd2757e66297fdd217817e7ab6e3a36b
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2020-06-06 13:36:24 +0300

    Move selected category name to a top of element, near the Category label

--- component/add_purchase/Category.js
+++ component/add_purchase/Category.js
@@ -8,26 +8,29 @@ import {
 import styles from "../../Styles";
 
 const Category = ({categories, category, setCategory}) => {
+	
+	const isCurrentCategory = (categoryId) => {
+		return category.id === categoryId;
+	}
 
 	return (
 		<View style={styles.sectionContainer}>
-			<Text style={styles.headersText}>Category</Text>
+			<Text style={styles.headersText}>Category is {!!category ? category.name : "not selected"}</Text>
 			<View style={{display: "flex", flexDirection: "row", flexWrap: "wrap"}}>
 				{
-					categories.map((icon, index) => (
+					categories.map((category) => (
 						<TouchableHighlight
-							key={icon.id}
-							onPress={() => setCategory(icon)}
+							key={category.id}
+							onPress={() => setCategory(category)}
 							style={{flex: 1, minWidth: 50, maxWidth: 50, marginLeft: 10, marginRight: 10}}
 						>
-							<View style={[category.id === icon.id ? styles.grayIcon : styles.blackIcon, {display: "flex", justifyContent: "center", textAlign: "center"}]}>
-								<Text style={{fontSize: 40, textAlign: "center"}}>{icon.emoji}</Text>
+							<View style={[isCurrentCategory(category.id) ? styles.grayIcon : styles.blackIcon, {display: "flex", justifyContent: "center", textAlign: "center"}]}>
+								<Text style={{fontSize: 40, textAlign: "center"}}>{category.emoji}</Text>
 							</View>
 						</TouchableHighlight>
 					))
 				}
 			</View>
-			<Text style={styles.headersText}>{category.name}</Text>
 		</View>
 	)
 }
