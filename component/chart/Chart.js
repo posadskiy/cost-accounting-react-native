a5186d67a2f7f662d8414d1375a0c87260dbbe7c
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2020-06-07 00:38:51 +0300

    Chart component with no logic yet

---
+++ component/chart/Chart.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+	View,
+	Text,
+} from 'react-native';
+import styles from "../../Styles";
+
+const Chart = () => {
+	return (
+		<View>
+			<Text style={styles.headersText}>Under developing</Text>
+		</View>
+	)
+}
+
+export default Chart;
