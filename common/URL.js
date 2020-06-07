e7f5d05a5bf2bfd6eb0ea705e6c0509ae9767298
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2020-06-07 12:15:45 +0300

    URLs to local and production back end

---
+++ common/URL.js
@@ -0,0 +1,40 @@
+const LOCAL = 0;
+const PRODUCTION = 1;
+const mode = LOCAL;
+
+const LOCAL_SERVER = "http://localhost:8080/";
+const PRODUCTION_SERVER = "https://cost-accounting.posadskiy.com/";
+const URL = {
+	addPurchase: (userId) => `purchase/add/${userId}`,
+	addIncome: (userId) => `income/add/${userId}`,
+	getCurrencies: 'purchase/currencies',
+	getCategories: 'category/allPurchases',
+	getLastPurchases: (userId) => `statistic/${userId}/lastPurchases`,
+	getLastIncomes: (userId) => `statistic/${userId}/lastIncomes`,
+}
+
+const url = (url) => {
+	let server;
+
+	switch (mode) {
+		case LOCAL: {
+			server = LOCAL_SERVER;
+			break;
+		}
+		case PRODUCTION: {
+			server = PRODUCTION_SERVER;
+			break;
+		}
+		default: {
+			server = LOCAL_SERVER;
+			break;
+		}
+	}
+
+	return server + url;
+}
+
+export {
+	URL,
+	url,
+}
