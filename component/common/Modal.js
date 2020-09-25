326c5a353c8d8b4dddbdc75589933d764ec046c0
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2020-09-25 11:39:58 +0300

    Global Modal view

---
+++ component/common/Modal.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Modal from 'react-native-modal';
+import styles from "../../Styles";
+import {Button, View} from "react-native";
+
+const BlackModal = ({children, isModalVisible, setIsModalVisible, onCloseModal}) => {
+
+  return (
+    <Modal
+      isVisible={isModalVisible}
+      backdropColor="black"
+      backdropOpacity={0.95}
+      animationIn="zoomInDown"
+      animationOut="zoomOutUp"
+      animationInTiming={600}
+      animationOutTiming={600}
+      backdropTransitionInTiming={600}
+      backdropTransitionOutTiming={600}
+    >
+      <View style={styles.sectionContainer}>
+        {children}
+        <View style={{display: "flex", flexDirection: "row", justifyContent: "center"}}>
+          <Button
+            onPress={onCloseModal}
+            title="Cancel"
+            style={{flex: 1}}
+          />
+          <Button
+            onPress={() => setIsModalVisible(false)}
+            title="Apply"
+            style={{flex: 1}}
+          />
+        </View>
+      </View>
+    </Modal>
+  );
+}
+
+export default BlackModal;
