93032ee408841737b929d91c9a0f89d5ab2abc6a
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2020-12-10 21:15:04 +0300

    User Page MVP

---
+++ component/user/EditableRow.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {Pressable, Text, View} from "react-native";
+import styles from "../../Styles";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+
+const EditableRow = ({header, value, onPress}) => {
+  return (
+    <Pressable onPress={onPress} style={{display: "flex", flexDirection: "row", padding: 5}}>
+      <Text style={[{flex: 1}, styles.headersText]}>{header}</Text>
+      <View style={{flex: 1, display: "flex", flexDirection: "row", justifyContent: "flex-end"}}>
+        <Text style={styles.generalTextRight}>{value}</Text>
+        <FontAwesome5 style={{alignSelf: "center", paddingLeft: 5}} color="white" name="angle-right" size={25} />
+      </View>
+    </Pressable>
+  )
+}
+
+export default EditableRow;
--- component/user/User.js
+++ component/user/User.js
@@ -1,15 +1,80 @@
-import React, {useState} from 'react';
+import React, {useState, useContext, useEffect} from 'react';
 import {
   View,
-  Text,
+  ScrollView,
 } from 'react-native';
+import {saveUser} from "../../actions/userActions";
 import styles from "../../Styles";
+import {UserContext} from "../login/Login";
+import EditableRow from "./EditableRow";
+import Button from "../common/Button";
+import EditableFieldModal from "../common/EditableFieldModal";
 
 const User = () => {
+  const user = useContext(UserContext);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [defaultCurrency, setDefaultCurrency] = useState(user.defaultCurrency);
+  const [name, setName] = useState(user.name);
+  const [modalText, setModalText] = useState();
+  const [modalPlaceholder, setModalPlaceholder] = useState();
+  const [modalHeader, setModalHeader] = useState();
+  const [modalOnChangeText, setModalOnChangeText] = useState({});
+
+  const openNameModal = () => showModal(name, "User name", "Your name", setName);
+  const openDefaultCurrencyModal = () => showModal(defaultCurrency, "Default currency", "USD", setDefaultCurrency);
+
+  const showModal = (text, header, placeholder, setText) => {
+    setModalText(text);
+    setModalHeader(header);
+    setModalPlaceholder(placeholder);
+    setModalOnChangeText({setText})
+    setIsModalVisible(true);
+  }
+  
+  const updateUser = async () => {
+    const body = {
+      id: user.id,
+      name,
+      defaultCurrency,
+    }
+    
+    console.log(user);
+    
+    await saveUser(body);
+  }
+  
   return (
-    <View style={styles.sectionContainer}>
-      <Text style={styles.generalText}>Under developing</Text>
-    </View>
+    <ScrollView
+      contentInsetAdjustmentBehavior="automatic"
+      style={styles.scrollView}
+    >
+      <EditableFieldModal
+        text={modalText}
+        setText={setModalText}
+        placeholder={modalPlaceholder}
+        header={modalHeader}
+        isVisible={isModalVisible}
+        setIsVisible={setIsModalVisible}
+        onChangeText={modalOnChangeText}
+      />
+      <View style={styles.sectionContainer}>
+        <EditableRow 
+          header="Name"
+          value={name}
+          onPress={openNameModal}
+        />
+        <EditableRow
+          header="Default currency"
+          value={defaultCurrency}
+          onPress={openDefaultCurrencyModal}
+        />
+        <Button
+          text="Save"
+          onPress={updateUser}
+          isEnabled={true}
+        />
+      </View>
+    </ScrollView>
   )
 }
 
